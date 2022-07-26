28eacbeacaa7c3ee81b94e8173d0f43a4fa182b4
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2022-07-26 16:31:44 +0530

    changes to rds and gitignore

--- rds.tf
+++ rds.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "sathya_rds" {
   allocated_storage    = 20
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0.28"
   instance_class       = "db.t3.micro"
-  name                 = "rds-inst"
+  db_name              = "rds-instance"
   username             = "lab-user"
   password             = "labuseradmin"
   parameter_group_name = "default.mysql5.7"
